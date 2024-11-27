7e3e1260b95f7d02469b524153972ad3607cffc8
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-11-27 04:47:14 +0000

    市区町村一覧画面のUIファイルを切り離し

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/city_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,31 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-//＝＝＝ CityListPage  ここから ＝＝＝
-
-class CityListPage extends StatelessWidget {
-  const CityListPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('市区町村一覧'),
-      ),
-      body: const Center(
-        child: Text('市区町村の一覧画面です'),
-      ),
-    );
-  }
-}
-
-//＝＝＝ CityListPage. ここまで ＝＝＝
-
-//＝＝＝ ここから ＝＝＝
-//＝＝＝ ここまで ＝＝＝
-
-//＝＝＝ ここから ＝＝＝
-//＝＝＝ ここまで ＝＝＝
