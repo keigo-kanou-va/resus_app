9ea17ad2aa4f1767e0364e62094b96edaf9c64a6
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-11-27 04:38:51 +0000

    CityListPageウィジェットを切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter RESAS App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('市区町村一覧'),
-        ),
-        body: const Center(
-          child: Text('市区町村の一覧画面です'),
-        ),
+      home: CityListPage(),
+    );
+  }
+}
+
+//＝＝＝ CityListPage  ここから ＝＝＝
+
+class CityListPage extends StatelessWidget {
+  const CityListPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('市区町村一覧'),
+      ),
+      body: const Center(
+        child: Text('市区町村の一覧画面です'),
       ),
     );
   }
 }
+
+//＝＝＝ CityListPage. ここまで ＝＝＝
+
+//＝＝＝ ここから ＝＝＝
+//＝＝＝ ここまで ＝＝＝
+
+//＝＝＝ ここから ＝＝＝
+//＝＝＝ ここまで ＝＝＝
