4f33dff4293dca7cb2081cf0d76fee24d337154f
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-11-28 05:52:08 +0000

    不要なcities配列を削除

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -36,29 +36,6 @@ class _CityListPageState extends State<CityListPage> {
 
   @override
   Widget build(BuildContext context) {
-    const cities = [
-      '札幌市',
-      '仙台市',
-      'さいたま市',
-      '千葉市',
-      '横浜市',
-      '川崎市',
-      '相模原市',
-      '新潟市',
-      '静岡市',
-      '浜松市',
-      '名古屋市',
-      '京都市',
-      '大阪市',
-      '堺市',
-      '神戸市',
-      '岡山市',
-      '広島市',
-      '北九州市',
-      '福岡市',
-      '熊本市',
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
