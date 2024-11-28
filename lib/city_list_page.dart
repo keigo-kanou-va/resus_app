0d4cce29dd88faa4f35d952004d1deb739ca5f82
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-11-28 05:44:38 +0000

    市区町村データをjsonパースしてUIに表示

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:myapp/city_detail_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:myapp/env.dart';
 
 class CityListPage extends StatefulWidget {
   const CityListPage({
@@ -11,13 +15,23 @@ class CityListPage extends StatefulWidget {
 }
 
 class _CityListPageState extends State<CityListPage> {
-  late Future<void> _future;
+  late Future<String> _future;
 
   //画面を開いた時の処理
   @override
   void initState() {
     super.initState();
-    _future = Future.delayed(const Duration(seconds: 3));
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = '/api/v1/cities';
+    final headers = {
+      'X-API-KEY': Env.resasApikey,
+    };
+    _future = http
+        .get(
+          Uri.https(host, endpoint),
+          headers: headers,
+        )
+        .then((res) => res.body);
   }
 
   @override
@@ -49,7 +63,7 @@ class _CityListPageState extends State<CityListPage> {
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: FutureBuilder<void>(
+      body: FutureBuilder<String>(
           future: _future,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -57,18 +71,21 @@ class _CityListPageState extends State<CityListPage> {
                 child: CircularProgressIndicator(),
               );
             }
+            final json = jsonDecode(snapshot.data!)['result'] as List;
+            final items = json.cast<Map<String, dynamic>>();
+
             return ListView(
               children: [
-                for (final city in cities)
+                for (final city in items)
                   ListTile(
-                    title: Text(city),
+                    title: Text(city['cityName']),
                     subtitle: const Text('政令指定都市'),
                     trailing: const Icon(Icons.navigate_next),
                     onTap: () {
                       Navigator.of(context).push<void>(
                         MaterialPageRoute(
                           builder: (context) => CityDetailPage(
-                            city: city,
+                            city: city['cityName'],
                           ),
                         ),
                       );
