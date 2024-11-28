81f7808a5a7f1ca9a8f1aea0e094628490360293
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-11-28 05:14:32 +0000

    非同期処理の追加

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/city_detail_page.dart';
 
-class CityListPage extends StatelessWidget {
+class CityListPage extends StatefulWidget {
   const CityListPage({
     super.key,
   });
+
+  @override
+  State<CityListPage> createState() => _CityListPageState();
+}
+
+class _CityListPageState extends State<CityListPage> {
+  late Future<void> _future;
+
+  //画面を開いた時の処理
+  @override
+  void initState() {
+    super.initState();
+    _future = Future.delayed(const Duration(seconds: 3));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,25 +49,34 @@ class CityListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: ListView(
-        children: [
-          for (final city in cities)
-            ListTile(
-              title: Text(city),
-              subtitle: const Text('政令指定都市'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {
-                Navigator.of(context).push<void>(
-                  MaterialPageRoute(
-                      builder: (context) => CityDetailPage(
-                        city: city,
-                      ),
-                  ),
-                );
-              },
-            )
-        ],
-      ),
+      body: FutureBuilder<void>(
+          future: _future,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView(
+              children: [
+                for (final city in cities)
+                  ListTile(
+                    title: Text(city),
+                    subtitle: const Text('政令指定都市'),
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(
+                            city: city,
+                          ),
+                        ),
+                      );
+                    },
+                  )
+              ],
+            );
+          }),
     );
   }
 }
